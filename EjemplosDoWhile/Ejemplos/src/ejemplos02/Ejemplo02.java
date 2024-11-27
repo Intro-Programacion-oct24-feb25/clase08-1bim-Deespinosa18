3c9195e8bac7c7d315ac4f0e36807f06cb92cf2d
Diego Espinosa <diegoeespinosag18@gmail.com>
2024-11-27 11:41:16 -0500

    cambio suave

--- EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo02.java
+++ EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo02.java
@@ -31,11 +31,22 @@ public class Ejemplo02 {
             cadenaFinal = String.format("%s%.2f\n", cadenaFinal, nota);
             entrada.nextLine(); // limpieza de buffer
 
-            System.out.println("Ingrese (s) si desea salir del ciclo ");
+            System.out.println("Ingrese (s), (yes), (y) o (si), si desea salir del ciclo ");
             salida = entrada.nextLine();
             
-            if (salida.equals ("si")|| salida.equals("s")
+            /*
+            switch (salida) {
+            case "s"
+            case "yes"
+            case "si"
+            case "y"
+                bandera = false
+            break;
+            tambien se puede utilizar el switch de esta manera para obt the same
+            */
+            if (salida.equals ("si") || salida.equals("s")
                     || salida.equals("yes") || salida.equals("y")){
+                bandera= false;
             }
             
             
